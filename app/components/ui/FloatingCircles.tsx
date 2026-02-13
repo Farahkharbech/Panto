d3922aee9c623ff9ce2d45be2c43486efaf62149
farahkharbech <farah.kh@hotmail.fr>
2026-02-13 17:50:49 +0100

    Floating

--- app/components/ui/FloatingCircles.tsx
+++ app/components/ui/FloatingCircles.tsx
@@ -1,24 +1,15 @@
 export default function FloatingCircles() {
   const circles = [
-    // Bottom right - 37px
+    // Left middle - 56px (moved up 30px, left 20px)
     {
-      size: 37,
-      top: '809px',
-      left: '1369px',
-      innerSize: 11.89,
-      innerLeft: '1381.55px',
-      innerTop: '821.55px',
+      size: 56,
+      top: '636.6px',
+      left: '267px',
+      innerSize: 18,
+      innerLeft: '26px',
+      innerTop: '655.6px',
     },
-    // Center - 32px
-    {
-      size: 32,
-      top: '777px',
-      left: '800px',
-      innerSize: 10.29,
-      innerLeft: '810.86px',
-      innerTop: '787.86px',
-    },
-    // Top left - 32px
+    // Top left - 32px (moved up 30px, left 20px)
     {
       size: 32,
       top: '743px',
@@ -27,12 +18,31 @@ export default function FloatingCircles() {
       innerLeft: '113.86px',
       innerTop: '753.86px',
     },
+    // Center - 32px (moved up 30px, left 20px)
+    {
+      size: 32,
+      top: '747px',
+      left: '770px',
+      innerSize: 10.29,
+      innerLeft: '780.86px',
+      innerTop: '757.86px',
+    },
+    // Bottom right - 37px (moved up 30px, left 50px)
+    {
+      size: 37,
+      top: '779px',
+      left: '1319px',
+      innerSize: 11.89,
+      innerLeft: '1331.55px',
+      innerTop: '791.55px',
+    },
   ];
 
   return (
     <>
       {circles.map((circle, index) => (
         <div key={index}>
+          {/* Outer Circle - Ellipse 212 */}
           <div
             className="absolute pointer-events-none hidden lg:block"
             style={{
@@ -49,6 +59,7 @@ export default function FloatingCircles() {
             }}
           />
 
+          {/* Inner White Circle - Ellipse 211 */}
           <div
             className="absolute pointer-events-none hidden lg:block"
             style={{
